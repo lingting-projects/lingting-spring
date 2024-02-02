6cd6b44251e15292c617ba6dfa24f28bf4670af9
lingting <sunlisten.gzm@gmail.com>
2024-02-02 16:39:07 +0800

    :wrench: 优化core模块依赖传递

--- lingting-spring-core/build.gradle.kts
+++ lingting-spring-core/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies{
     api("live.lingting.framework:lingting-core")
 
-    implementation("org.aspectj:aspectjweaver")
+    compileOnly("org.aspectj:aspectjweaver")
 
     api("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-autoconfigure")
-    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    api("org.springframework.boot:spring-boot-autoconfigure")
+    api("org.springframework.boot:spring-boot-configuration-processor")
 }
