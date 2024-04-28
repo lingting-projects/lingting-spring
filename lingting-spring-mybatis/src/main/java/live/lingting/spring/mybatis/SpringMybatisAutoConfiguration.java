d6762298679c12d073243af8e6cce56643c58087
lingting <sunlisten.gzm@foxmail.com>
2024-04-28 10:57:58 +0800

    :sparkles: mybatis支持自动注册扩展

--- lingting-spring-mybatis/src/main/java/live/lingting/spring/mybatis/SpringMybatisAutoConfiguration.java
+++ lingting-spring-mybatis/src/main/java/live/lingting/spring/mybatis/SpringMybatisAutoConfiguration.java
@@ -1,6 +1,7 @@
 package live.lingting.spring.mybatis;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
@@ -10,7 +11,9 @@ import live.lingting.framework.datascope.handler.DefaultDataPermissionHandler;
 import live.lingting.framework.datascope.parser.DataScopeParser;
 import live.lingting.framework.datascope.parser.DefaultDataScopeParser;
 import live.lingting.framework.mybatis.datascope.DataPermissionInterceptor;
+import live.lingting.framework.mybatis.typehandler.AutoRegisterTypeHandler;
 import live.lingting.spring.jackson.ObjectMapperAfter;
+import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -63,6 +66,16 @@ public class SpringMybatisAutoConfiguration {
 	@ConditionalOnMissingBean
 	public ExtendServiceImplBeanPost extendServiceImplBeanPost() {
 		return new ExtendServiceImplBeanPost();
+	}
+
+	@Bean
+	public ConfigurationCustomizer autoRegisterCOnfigurationCustomizer(List<AutoRegisterTypeHandler<?>> list) {
+		return configuration -> {
+			TypeHandlerRegistry registry = configuration.getTypeHandlerRegistry();
+			for (AutoRegisterTypeHandler<?> handler : list) {
+				registry.register(handler);
+			}
+		};
 	}
 
 }
