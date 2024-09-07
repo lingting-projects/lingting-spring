a00874b0656d4451748a2f6a1457aefdae34e519
lingting <sunlisten.gzm@foxmail.com>
2024-09-07 11:31:13 +0800

    :zap: spring web 校验开启快速失败

--- lingting-spring-web/src/main/java/live/lingting/spring/web/configuration/SpringWebAutoConfiguration.java
+++ lingting-spring-web/src/main/java/live/lingting/spring/web/configuration/SpringWebAutoConfiguration.java
@@ -5,24 +5,32 @@ import live.lingting.spring.web.filter.WebScopeFilter;
 import live.lingting.spring.web.properties.SpringWebProperties;
 import live.lingting.spring.web.resolve.ApiPaginationParamsResolve;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.EnumSet;
 import java.util.List;
 
+import static org.hibernate.validator.BaseHibernateValidatorConfiguration.FAIL_FAST;
+
 /**
  * @author lingting 2024-03-20 14:59
  */
-@AutoConfiguration
+@AutoConfigureBefore(ValidationAutoConfiguration.class)
 @EnableConfigurationProperties(SpringWebProperties.class)
+@AutoConfiguration(before = ValidationAutoConfiguration.class)
 public class SpringWebAutoConfiguration {
 
 	@Bean
@@ -51,6 +59,13 @@ public class SpringWebAutoConfiguration {
 				resolvers.add(resolve);
 			}
 		};
+	}
+
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	public ValidationConfigurationCustomizer validationConfigurationCustomizer() {
+		return configuration -> configuration.addProperty(FAIL_FAST, "true");
 	}
 
 }
