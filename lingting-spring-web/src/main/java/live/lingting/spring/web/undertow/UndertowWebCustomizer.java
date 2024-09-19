61a78f1b685a7e468ab75dc37720a564498df67f
lingting <sunlisten.gzm@foxmail.com>
2024-09-19 14:10:48 +0800

    :zap: 启用虚拟线程时 undertow 使用虚拟线程

--- lingting-spring-web/src/main/java/live/lingting/spring/web/undertow/UndertowWebCustomizer.java
+++ lingting-spring-web/src/main/java/live/lingting/spring/web/undertow/UndertowWebCustomizer.java
@@ -1,24 +1,45 @@
 package live.lingting.spring.web.undertow;
 
 import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import live.lingting.framework.thread.VirtualThread;
+import live.lingting.framework.util.BooleanUtils;
+import live.lingting.spring.util.EnvironmentUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
+
+import java.util.concurrent.ExecutorService;
 
 /**
  * @author lingting 2024-03-20 16:01
  */
-public class UndertowWebCustomizer implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+@Slf4j
+public class UndertowWebCustomizer implements UndertowDeploymentInfoCustomizer {
 
 	@Override
-	public void customize(UndertowServletWebServerFactory factory) {
+	public void customize(DeploymentInfo info) {
+		WebSocketDeploymentInfo websocket = websocket();
+		info.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", websocket);
+		ExecutorService executor = executor();
+		if (executor != null) {
+			info.setExecutor(executor);
+			info.setAsyncExecutor(executor);
+		}
+	}
+
+	protected WebSocketDeploymentInfo websocket() {
 		DefaultByteBufferPool buffers = new DefaultByteBufferPool(false, 2048, -1, 24, 0);
 
 		WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
 		webSocketDeploymentInfo.setBuffers(buffers);
+		return webSocketDeploymentInfo;
+	}
 
-		factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo
-			.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo));
+	protected ExecutorService executor() {
+		String property = EnvironmentUtils.getProperty("spring.threads.virtual.enabled");
+		boolean enabled = BooleanUtils.isTrue(property);
+		return enabled ? VirtualThread.executor() : null;
 	}
 
 }
