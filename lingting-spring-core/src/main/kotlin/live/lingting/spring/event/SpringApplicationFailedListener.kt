71374877a922fcfc47a286c5f4ec6410797c1095
lingting <sunlisten.gzm@foxmail.com>
2025-01-03 14:15:48 +0800

    :bug: 修复SpringApplicationFailedListener监听事件异常

--- lingting-spring-core/src/main/kotlin/live/lingting/spring/event/SpringApplicationFailedListener.kt
+++ lingting-spring-core/src/main/kotlin/live/lingting/spring/event/SpringApplicationFailedListener.kt
@@ -2,7 +2,7 @@ package live.lingting.spring.event
 
 import live.lingting.framework.application.ApplicationHolder
 import live.lingting.framework.util.Slf4jUtils.logger
-import org.springframework.boot.context.event.ApplicationStartingEvent
+import org.springframework.boot.context.event.ApplicationFailedEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -11,13 +11,13 @@ import org.springframework.core.annotation.Order
  * @author lingting 2023-12-06 17:18
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class SpringApplicationFailedListener : ApplicationListener<ApplicationStartingEvent> {
+class SpringApplicationFailedListener : ApplicationListener<ApplicationFailedEvent> {
 
     companion object {
         private val log = logger()
     }
 
-    override fun onApplicationEvent(event: ApplicationStartingEvent) {
+    override fun onApplicationEvent(event: ApplicationFailedEvent) {
         log.debug("spring application failed")
         ApplicationHolder.stop()
     }
