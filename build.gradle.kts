961e6f1f1b144b123ed732bb1c14ad265ce0abd5
lingting <sunlisten.gzm@foxmail.com>
2025-01-17 15:53:58 +0800

    :rocket: 2024.11.21-Bata-6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 val projectGroup = "live.lingting.spring"
-val projectVersion = "2024.11.21-Bata-6"
+val projectVersion = "2024.11.21-Beta-1"
 
 // 用于子模块获取包管理信息
 val catalogLibs = libs
