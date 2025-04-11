a7650beb20dbe4ccb1151e7b391ab2e4f0ce1c8a
lingting <sunlisten.gzm@foxmail.com>
2025-04-11 11:46:08 +0800

    :zap: 优化 Application启动时机. 提前到 Prepared

--- lingting-spring-core/src/main/kotlin/live/lingting/spring/event/SpringApplicationPreparedListener.kt
+++ lingting-spring-core/src/main/kotlin/live/lingting/spring/event/SpringApplicationPreparedListener.kt
@@ -2,7 +2,7 @@ package live.lingting.spring.event
 
 import live.lingting.framework.application.ApplicationHolder
 import live.lingting.framework.util.Slf4jUtils.logger
-import org.springframework.boot.context.event.ApplicationStartingEvent
+import org.springframework.boot.context.event.ApplicationPreparedEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -11,13 +11,13 @@ import org.springframework.core.annotation.Order
  * @author lingting 2023-12-06 17:18
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class SpringApplicationStartingListener : ApplicationListener<ApplicationStartingEvent> {
+class SpringApplicationPreparedListener : ApplicationListener<ApplicationPreparedEvent> {
     companion object {
         private val log = logger()
     }
 
-    override fun onApplicationEvent(event: ApplicationStartingEvent) {
-        log.debug("spring application starting")
+    override fun onApplicationEvent(event: ApplicationPreparedEvent) {
+        log.debug("spring application prepared")
         ApplicationHolder.start()
     }
 }
--- lingting-spring-core/src/test/kotlin/live/lingting/spring/SpringTest.kt
+++ lingting-spring-core/src/test/kotlin/live/lingting/spring/SpringTest.kt
@@ -1,17 +1,22 @@
 package live.lingting.spring
 
+import live.lingting.framework.concurrent.Await
+import live.lingting.framework.thread.AbstractTimer
+import live.lingting.framework.util.DurationUtils.seconds
 import live.lingting.spring.util.EnvironmentUtils
 import live.lingting.spring.util.SpringUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.stereotype.Component
 
 /**
  * @author lingting 2024-02-02 13:54
  */
 @SpringBootTest
 class SpringTest {
+
     @Test
     fun test() {
         assertTrue(EnvironmentUtils.isActiveProfiles("test"))
@@ -20,5 +25,19 @@ class SpringTest {
         val bean = beans.values.iterator().next()
         assertEquals("bean", bean.value)
         assertTrue(bean.isStart)
+        val timer = SpringUtils.getBean(Timer::class)
+        Await.wait(1.seconds())
+        assertEquals(1, timer.count)
     }
+}
+
+
+@Component
+class Timer : AbstractTimer() {
+    var count = 0
+
+    override fun process() {
+        count += 1
+    }
+
 }
